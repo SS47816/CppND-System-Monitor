b6319937f80e0797b33365f0eeb27dafc6732fec
SS47816 <ss1057532022@gmail.com>
2020-06-03 06:23:35 +0800

    feat: add Format::ElapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,10 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  const auto hours = seconds / 3600;
+  const auto minutes = (seconds % 3600) / 60;
+  const auto remainder = seconds % 60;
+  const string time = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(remainder);
+  return time;
+}
