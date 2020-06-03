fcf1f94f195b57fee81a927232b1184a46a6c9dc
SS47816 <ss1057532022@gmail.com>
2020-06-04 03:08:22 +0800

    feat: beautified Format::ElapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -9,9 +9,14 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
-  const auto hours = seconds / 3600;
-  const auto minutes = (seconds % 3600) / 60;
-  const auto remainder = seconds % 60;
-  const string time = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(remainder);
-  return time;
+  const auto hours = int(seconds / 3600);
+  auto remainder = seconds - hours * 3600;
+  const auto minutes = remainder / 60;
+  remainder = remainder - minutes * 60;
+
+  string hh = hours >= 10? std::to_string(hours) : "0" + std::to_string(hours);
+  string mm = minutes >= 10? std::to_string(minutes) : "0" + std::to_string(minutes);
+  string ss = remainder >= 10? std::to_string(remainder) : "0" + std::to_string(remainder);
+  
+  return hh + ":" + mm + ":" + ss;
 }
